e273d7e286dd26aae6a1ab39ede5cb26101929c0
= <jochanolm@gmail.com>
2024-10-21 19:55:33 -0300

    extractor complete

--- pipelines/fetch-farcaster-data/fetch.py
+++ pipelines/fetch-farcaster-data/fetch.py
@@ -23,10 +23,9 @@ class FetchFarcasterHubData:
         self.AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
         self.BUCKET_NAME = os.getenv('BUCKET_NAME')
         self.data = {}
-        self.runtime = datetime.now()
-        self.manual_cutoff = os.getenv('CUTOFF')
-        self.channel_fids = json.loads(os.getenv('FIDS', '[]'))
-        print(type(self.channel_fids))
+        self.runtime = datetime.now().strftime("%Y-%m-%d-%H-%M")
+        self.cutoff = os.getenv('CUTOFF')
+        self.channels = json.loads(os.getenv('CHANNEL_IDS', '[]'))
         
         self.s3_client = boto3.client(
             's3', 
@@ -36,11 +35,15 @@ class FetchFarcasterHubData:
         )
 
         logging.basicConfig(level=logging.INFO)
-        self.logger = logging.getLogger(__name__)
-        self.metadata = helpers.load_metadata(self.s3_client, self.BUCKET_NAME, self.logger)
-        self.last_run = self.metadata.get('last_run', None)
-
-        
+        self.data = {
+                    'members': [],
+                    'followers': [],
+                    'all_channel_fids': [],
+                    'all_channel_casts': [],
+                    'followed_channels': []
+                }
+                
+                
     def get_channel_followers(self, channel_id):
         print(f"Fetching followers for channel {channel_id}...")
         headers = {
@@ -87,8 +90,9 @@ class FetchFarcasterHubData:
 
     def get_all_fids_channel_members(self, channel_members):
         fids = set()
-        for member in channel_members:
-            fids.add(member.get('user').get('fid'))
+        for member in channel_members[0:5]:
+            print(member)
+            fids.add(member.get('fid'))
         return list(fids)
         
 
@@ -120,23 +124,40 @@ class FetchFarcasterHubData:
             self.logger.error(f"Error fetching channel metadata: {e}")
             return None
         
-    def get_user_casts_in_channel(self, fid, channel):
-        self.logger.info(f"Collecting casts for user {fid} in channel {channel}.....")
+    def get_user_casts_in_channel(self, channel, fids):
         endpoint = "feed/user/casts"
-        params = {'fid': fid, 
-                  'api_key': self.NEYNAR_API_KEY,
-                  'limit': 150,
-                  'channel_fid': channel}
+        all_filtered_casts = []
+        cutoff_date = datetime.now(timezone.utc) - timedelta(days=int(self.manual_cutoff))
         headers = {
             'accept': 'application/json',
             'api_key': self.NEYNAR_API_KEY
         }
-        try:
-            casts_in_channel = helpers.query_neynar_api(endpoint, params, headers)
-            return casts_in_channel
-        except Exception as e:
-            self.logger.error(f"Error fetching casts in channel {channel} for user: {e}")
-            return None
+
+        for fid in fids:
+            self.logger.info(f"Collecting casts for user {fid} in channel {channel}.....")
+            params = {
+                'fid': fid, 
+                'api_key': self.NEYNAR_API_KEY,
+                'limit': 150,
+                'channel_fid': channel
+            }
+            try:
+                casts_in_channel = helpers.query_neynar_api(endpoint, params, headers)
+                for cast in casts_in_channel:
+                    if 'timestamp' in cast:
+                        try:
+                            cast_timestamp = datetime.strptime(cast['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=timezone.utc)
+                        except ValueError:
+                            # If the above fails, try parsing without milliseconds
+                            cast_timestamp = datetime.strptime(cast['timestamp'], '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
+                        print(f"Cast timestamp: {cast_timestamp}")
+                        if cast_timestamp >= cutoff_date:
+                            all_filtered_casts.append(cast)
+            except Exception as e:
+                self.logger.error(f"Error fetching casts in channel {channel} for user {fid}: {e}")
+                continue
+
+        return all_filtered_casts
         
     def get_all_user_channels(self, fids):
         for fid in fids:
@@ -156,15 +177,30 @@ class FetchFarcasterHubData:
             except Exception as e:
                 self.logger.error(f"Error fetching additional channels for user {fid}")
                 return None 
-        
-
+            
 
 
         
     def run(self):
-        for channel_id in self.channel_fids:
-            members = self.get_channel_members(channel_id)
-            self.data['members'] = members 
+        for channel in self.channels:
+            channel_dict = self.channel_dict = {}
+            channel_dict['channel_metadata'] = self.get_channel_metadata(channel)
+            members = self.get_channel_members(channel)
+            followers = self.get_channel_followers(channel)
+            all_channel_fids = self.get_all_channel_fids(members, followers)
+            channel_dict['members'] = members 
+            channel_dict['followers'] = followers 
+            channel_dict['all_channel_fids'] = all_channel_fids
+            channel_dict['casts'] = self.get_user_casts_in_channel(channel, all_channel_fids)
+            channel_dict['all_channels'] = self.get_all_user_channels(all_channel_fids)
+            self.data['channels_data'] = channel_dict
+            helpers.save_data(
+                self.s3_client,
+                self.BUCKET_NAME,
+                "data_" + "farcaster_" + str(self.runtime) + '.json',
+                self.data
+            )
+
 
 if __name__ == "__main__":
     fethcer = FetchFarcasterHubData()
--- pipelines/fetch-farcaster-data/helpers.py
+++ pipelines/fetch-farcaster-data/helpers.py
@@ -13,46 +13,14 @@ from botocore.exceptions import ClientError
 #     """Converts Farcaster Hub Timestamp to UTC datetime"""
 #     return epoch + timedelta(seconds=int(timestamp))
 
-def save_data(s3_client, bucket_name, key, data, logger):
+def save_data(s3_client, bucket_name, key, data):
     """Saves data to S3"""
     try:
         s3_client.put_object(Bucket=bucket_name, Key=key, Body=json.dumps(data))
-        logger.info(f"Data saved to {key}")
+        logging.info(f"Data saved to {key}")
     except ClientError as e:
-        logger.error(f"Failed to save data: {e}")
+        logging.error(f"Failed to save data: {e}")
 
-def save_metadata(s3_client, bucket_name, metadata, logger):
-    """Saves metadata to S3"""
-    metadata['last_run'] = datetime.utcnow().isoformat()
-    try:
-        s3_client.head_bucket(Bucket=bucket_name)
-    except ClientError as e:
-        if e.response['Error']['Code'] == '404':
-            logger.info(f"Bucket {bucket_name} does not exist. Creating...")
-            try:
-                s3_client.create_bucket(Bucket=bucket_name)
-                logger.info(f"Bucket {bucket_name} created successfully.")
-            except ClientError as create_error:
-                logger.error(f"Failed to create bucket: {create_error}")
-                return
-        else:
-            logger.error(f"Error checking bucket: {e}")
-            return
-    save_data(s3_client, bucket_name, 'metadata.json', metadata, logger)
-
-def load_metadata(s3_client, bucket_name, logger):
-    """Loads metadata from S3"""
-    try:
-        obj = s3_client.get_object(Bucket=bucket_name, Key='metadata.json')
-        metadata = json.loads(obj['Body'].read().decode('utf-8'))
-        logger.info("Metgadata loaded.")
-        return metadata
-    except s3_client.exceptions.NoSuchKey:
-        logger.info("No metadata found. Starting fresh.")
-        return {}
-    except ClientError as e:
-        logger.error(f"Failed to load metadata: {e}")
-        return {}
     
 def query_neynar_hub(endpoint, params=None):
     base_url = "https://hub-api.neynar.com/v1/"
@@ -102,8 +70,8 @@ def query_neynar_api(endpoint, params, headers):
     url = f"{base_url}{endpoint}"
     all_data = []
 
-    # Make a copy to avoid mutating the original params
     current_params = params.copy()
+    logging.info(f"Retrieving data from {endpoint}...")
 
     while True:
         try:
@@ -111,30 +79,23 @@ def query_neynar_api(endpoint, params, headers):
             response.raise_for_status()
             data = response.json()
 
-            # Extract data from the first response key
             first_key = next(iter(data), None)
             if first_key is None:
-                print("No data in response. Exiting loop.")
                 break
 
             extracted_data = data.get(first_key, [])
-            if isinstance(extracted_data, list):
-                all_data.extend(extracted_data)
-            else:
-                break
+            all_data.extend(extracted_data)
+            logging.info(f"Retrieved {len(all_data)} objects total for endpoint {endpoint}...")
 
-            # Retrieve the cursor for the next page
             next_info = data.get('next', {})
             cursor = next_info.get('cursor')
 
             if not cursor:
                 break
 
-            # Add 'cursor' to current_params for the next request
             current_params['cursor'] = cursor
 
         except r.RequestException as e:
-            print(f"Request failed: {str(e)[:200]}")
             if hasattr(e.response, 'status_code'):
                 if e.response.status_code == 400:
                     print(f"Bad request. Response content: {e.response.text[:200]}")
@@ -142,5 +103,4 @@ def query_neynar_api(endpoint, params, headers):
                     print("Endpoint not found. Exiting loop.")
             break
 
-    print("Data collection completed successfully.")
     return all_data
